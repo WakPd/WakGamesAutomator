b1aa6b8bec45f25c6a40889e3b1b114754d9cb01
WakPd <germainkillian123@gmail.com>
2023-03-01 11:55:57 +0100

    new functions, somes fixes are coming

--- modules/nikke.py
+++ modules/nikke.py
@@ -12,7 +12,7 @@ if info.exists:
 
 device = uiautomator2.connect(info)
 
-def lobby_text():
+def nikke_lobby():
     lobby_txt = Text
     lobby_txt.TextMatch('Lobby')
 
@@ -20,5 +20,24 @@ def nikke_rung():
     device.app_start("com.proximabeta.nikke", "com.shiftup.nk.MainActivity")
     to_text = Text
     to_text.TextMatch('TO')
-    lobby_text()
-    
+    cross_img = 'assets/Nikke/cross.png'
+    cross_btn = Button
+    cross_btn.Match(cross_img)
+    nikke_lobby()
+    print("End of the process")
+
+def nikke_shop():
+    shop_txt = Text
+    shop_txt.TextMatch('Shop')
+
+def nikke_sale():
+    nikke_shop()
+    sale_btn_img = 'assets/Nikke/sale.png'
+    sale_btn = Button
+    sale_btn.Match(sale_btn_img)
+    buy_btn_img = 'assets/Nikke/buy.png'
+    buy_btn = Button
+    buy_btn.Match(buy_btn_img)
+    tap_to_claim_txt = Text
+    tap_to_claim_txt.TextMatch('Tap')
+    print("End of the process")
